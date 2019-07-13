85f7baa98d5c5b203f4cbe20ff76f4bc8cb43878
Scott Wren <cynics0xff@gmail.com>
2019-07-13 01:24:30 -0400

    Add spread operator examples

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
-   
+//output a color to the console 
 //console.log('%c Loading...', 'color: red');
 
+//create a simple object
 const animal = {
     name: 'Bob',
     legs: 4,
     type: 'canine',
     energy: 5
 }
+
+const p = { name: 'Picture' };
+const s = { energy: 50, speed: 30 };
+
+//use the spread 
+const ps = { ...p, ...s};
+
+let xyz = [ 'one', 'two', 'three', 'four'];
+
+//spread operator to insert xyz array and appendd the data
+let newxyz = [...xyz, 'Bob', 'John'];
+
+console.log(newxyz);
+
+console.log(ps);
 
 //destructoring
 function feed(animal) {
